bedd227f5b1f8c7d4be6011df5c58f24c8e9d28b
Elouan <elouan.argouarch@gmail.com>
2023-04-14 00:21:24 +0200

    wip

--- utils/visual.py
+++ utils/visual.py
@@ -58,16 +58,16 @@ purple_cmap = ListedColormap(purple[::-1])
 
 purple_color = "#660066"
 
-def plot_1d_unormalized_function(f,range = [-10,10], bins=100, figsize = (10,6), show = True):
+def plot_1d_unormalized_function(f,range = [-10,10], bins=100, figsize = (10,6), show = True, label = None):
     tt =torch.linspace(range[0],range[1],bins)
     with torch.no_grad():
         values = f(tt)
-    plot_1d_unormalized_values(values,tt, figsize = figsize, show = show)
+    plot_1d_unormalized_values(tt,values, figsize = figsize, show = show, label = label)
 
-def plot_1d_unormalized_values(values,tt, figsize = (10,6), show = True):
+def plot_1d_unormalized_values(tt,values, figsize = (10,6), show = True, label = None):
     x_min, x_max, bins = tt[0], tt[-1], tt.shape[0]
     fig = plt.figure(figsize = figsize)
-    plt.plot(tt, values*bins/(torch.sum(values)*(x_max - x_min)))
+    plt.plot(tt, values*bins/(torch.sum(values)*(x_max - x_min)), label = label)
     if show:
         plt.show()
 
