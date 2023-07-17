9c3279904b5edfe12a5fb07f025653b58fcd56f9
EA264728 <elouan.argouarch@cea.fr>
2023-07-17 15:57:01 +0200

    fix discriminative_regression.py

--- regressor/discriminative_regression.py
+++ regressor/discriminative_regression.py
@@ -106,12 +106,13 @@ class discriminative_bayesian_affine_regression:
         for _ in pbar:
             mean_x0_given_y0_beta_sigma2, sigma2_x0_given_y0_beta_sigma2 = self.compute_x0_given_y0_beta_sigma2_moments(
                 y0, current_beta, current_sigma2)
-            current_x0 = torch.distributions.Normal(mean_x0_given_y0_beta_sigma2,
-                                                    torch.sqrt(sigma2_x0_given_y0_beta_sigma2)).sample()
+            #current_x0 = torch.distributions.Normal(mean_x0_given_y0_beta_sigma2,torch.sqrt(sigma2_x0_given_y0_beta_sigma2)).sample()
+            current_x0 = (mean_x0_given_y0_beta_sigma2 + torch.sqrt(sigma2_x0_given_y0_beta_sigma2)*torch.randn(1)).squeeze(-1)
             current_labels = []
             for yj in Y:
                 mu_xj_given_yj_beta_sigma2,sigma2_xj_given_yj_beta_sigma2  = self.compute_x0_given_y0_beta_sigma2_moments(yj, current_beta, current_sigma2)
-                current_label = torch.distributions.Normal(mu_xj_given_yj_beta_sigma2,torch.sqrt(sigma2_xj_given_yj_beta_sigma2)).sample()
+                #current_label = torch.distributions.Normal(mu_xj_given_yj_beta_sigma2,torch.sqrt(sigma2_xj_given_yj_beta_sigma2)).sample()
+                current_label = (mu_xj_given_yj_beta_sigma2 + torch.sqrt(sigma2_xj_given_yj_beta_sigma2)*torch.randn(1)).squeeze(-1)
                 current_labels.append(current_label.repeat(yj.shape[0]))
             if torch.flatten(Y).shape[0] > 0:
                 DXplus = torch.cat([DX, current_x0.repeat(y0.shape[0]), torch.cat(current_labels)], dim=0)
@@ -119,8 +120,8 @@ class discriminative_bayesian_affine_regression:
                 DXplus = torch.cat([DX, current_x0.repeat(y0.shape[0])])
             mean_beta_given_Dplus, Sigma_beta_given_Dplus = self.compute_beta_given_sigma2_D_moments(current_sigma2,
                                                                                                      DXplus, DYplus)
-            current_beta = torch.distributions.MultivariateNormal(mean_beta_given_Dplus,
-                                                                  Sigma_beta_given_Dplus).sample()
+            #current_beta = torch.distributions.MultivariateNormal(mean_beta_given_Dplus,Sigma_beta_given_Dplus).sample()
+            current_beta = (mean_beta_given_Dplus + torch.linalg.cholesky(Sigma_beta_given_Dplus)@torch.randn(2)).squeeze(-1)
             current_sigma2 = self.compute_sigma2_given_beta_D_parameters(current_beta, DXplus, DYplus)
             list_x0_gibbs.append(current_x0)
             list_beta_gibbs.append(current_beta)
