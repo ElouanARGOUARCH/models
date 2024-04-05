94f13e2074124f6dca1c8c30435933a29ccc5604
Elouan <elouan.argouarch@gmail.com>
2024-04-05 17:14:59 +0200

    small fix

--- targets/datasets.py
+++ targets/datasets.py
@@ -6,7 +6,6 @@ def shuffle(tensor, randperm=None):
         randperm = torch.randperm(tensor.shape[0])
     return tensor[randperm], randperm
 
-
 def get_MNIST_dataset(one_hot = False,repository = 'C:\\Users\\Elouan\\PycharmProjects\\models\\targets\\data'):
     mnist_trainset = datasets.MNIST(root=repository, train=True,
                                     download=True, transform=None)
@@ -22,8 +21,6 @@ def get_MNIST_dataset(one_hot = False,repository = 'C:\\Users\\Elouan\\PycharmPr
         return torch.cat([train_samples, test_samples], dim = 0), torch.nn.functional.one_hot(torch.cat([train_labels,test_labels], dim = 0))
     else:
         return torch.cat([train_samples, test_samples], dim = 0), torch.cat([train_labels,test_labels], dim = 0)
-
-
 
 def get_FashionMNIST_dataset(one_hot = False,repository = 'C:\\Users\\Elouan\\PycharmProjects\\models\\targets\\data'):
     fmnist_trainset = datasets.FashionMNIST(root=repository, train=True,
@@ -51,9 +48,8 @@ def get_DIGITS_dataset(one_hot = False):
     else:
         return (samples + torch.rand_like(samples))/17, torch.tensor(labels)
 
-
 from ucimlrepo import fetch_ucirepo
-def get_WineQuality_Dataset(one_hot = False):
+def get_WineQuality_dataset(one_hot = False):
     wine_quality = fetch_ucirepo(id=186)
     samples = wine_quality.data.features
     labels = wine_quality.data.targets
@@ -61,3 +57,40 @@ def get_WineQuality_Dataset(one_hot = False):
         return torch.tensor(samples), torch.nn.functional.one_hot(torch.tensor(labels))
     else:
         return torch.tensor(samples),torch.tensor(labels)
+
+from keras.datasets import cifar10, cifar100
+import matplotlib.pyplot as plt
+def get_CIFAR10_dataset(one_hot = False, visual = False):
+    (trainX, trainy), (testX, testy) = cifar10.load_data()
+    samples = torch.cat([torch.tensor(trainX),torch.tensor(testX)], dim = 0)
+    labels = torch.cat([torch.tensor(trainy),torch.tensor(testy)], dim = 0).squeeze(-1).long()
+    samples = samples.reshape(samples.shape[0], 32*32*3)
+    if one_hot:
+        labels = torch.nn.functional.one_hot(labels,num_classes =10)
+    if visual:
+        for i in range(9):
+            # define subplot
+            plt.subplot(330 + 1 + i)
+            # plot raw pixel data
+            plt.imshow(trainX[i])
+        # show the figure
+        plt.show()
+    return samples, labels
+import ssl
+ssl._create_default_https_context = ssl._create_unverified_context
+def get_CIFAR100_dataset(one_hot = False, visual = False):
+    (trainX, trainy), (testX, testy) = cifar100.load_data()
+    samples = torch.cat([torch.tensor(trainX),torch.tensor(testX)], dim = 0)
+    labels = torch.cat([torch.tensor(trainy),torch.tensor(testy)], dim = 0).squeeze(-1).long()
+    samples = samples.reshape(samples.shape[0], 32*32*3)
+    if one_hot:
+        labels = torch.nn.functional.one_hot(labels,num_classes =100)
+    if visual:
+        for i in range(9):
+            # define subplot
+            plt.subplot(330 + 1 + i)
+            # plot raw pixel data
+            plt.imshow(trainX[i])
+        # show the figure
+        plt.show()
+    return samples, labels
